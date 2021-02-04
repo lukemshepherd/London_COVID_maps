094aa0a474393c3bf354118d8fcd884a4f1da32f
lukemshepherd <l.m.shepherd@outlook.com>
2021-02-04 08:46:04 +0000

    new docs

--- London_COVID_maps/_nbdev.py
+++ London_COVID_maps/_nbdev.py
@@ -6,8 +6,8 @@ index = {}
 
 modules = []
 
-doc_url = "https://{lukemshepherd}.github.io/London_COVID_maps/"
+doc_url = "https://lukemshepherd.github.io/London_COVID_maps/"
 
-git_url = "https://github.com/{lukemshepherd}/London_COVID_maps/tree/{branch}/"
+git_url = "https://github.com/lukemshepherd/London_COVID_maps/tree/master/"
 
 def custom_doc_links(name): return None
